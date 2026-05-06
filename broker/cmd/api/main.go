f6f60efc4fdc2685bbc0817d04d67a21f060d9ff
Seraphim325 <alexey.grishin325@gmail.com>
2026-05-06 19:59:46 +0300

    can be now run from docker

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"broker/cmd/api/router"
 	"broker/cmd/api/server"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	loadConfig()
+
 	r := router.Router{}
 	srv := server.Server{
-		Port:    "8080",
+		Port:    os.Getenv("BROKER_PORT"),
 		Handler: r.Handler(),
 	}
 
 	srv.Run()
+}
+
+func loadConfig() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Panic(err)
+	}
 }
